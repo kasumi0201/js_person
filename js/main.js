f972ecd0b2663cb317501618fe85866f7ec9721f
kasumi <kasumi0201@gmail.com>
2017-11-20 22:23:00 +1100

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var user = {
+firstname: "kasumi",
+lastname: "takeshima",
+greet: function(){
+  console.log("hello, my name is " + this.firstname + " "+ this.lastname);
+}
+};
+user.greet();
+
+
+console.log("Hello World");
